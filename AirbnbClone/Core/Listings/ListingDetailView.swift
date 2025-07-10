95cd66bd267b2019ff8d1e17efbb62b94f08cc4b
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-10 06:26:54 -0400

    feat(listing): add amenities section to ListingDetailView

--- AirbnbClone/Core/Listings/ListingDetailView.swift
+++ AirbnbClone/Core/Listings/ListingDetailView.swift
@@ -99,6 +99,7 @@ struct ListingDetailView: View {
             
             Divider()
             
+            // bedrooms view
             VStack(alignment: .leading, spacing: 16) {
                 Text("Where you will sleep")
                     .font(.headline)
@@ -122,6 +123,27 @@ struct ListingDetailView: View {
                     }
                 }
                 .scrollTargetBehavior(.paging)
+            }
+            .padding()
+            
+            Divider()
+            
+            // listing amenities
+            VStack(alignment: .leading, spacing: 16) {
+                Text("What this place offers")
+                    .font(.headline)
+                
+                ForEach(0 ..< 5) { feature in
+                    HStack {
+                        Image(systemName: "wifi")
+                            .frame(width: 32)
+                        
+                        Text("Wifi")
+                            .font(.footnote)
+                        
+                        Spacer()
+                    }
+                }
             }
             .padding()
         }
