6d4c8ad3fb7e9ae84ce54f391f307de5ab9f1966
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-10 05:15:08 -0400

    feat(listing): add host info section to ListingDetailView

--- AirbnbClone/Core/Listings/ListingDetailView.swift
+++ AirbnbClone/Core/Listings/ListingDetailView.swift
@@ -47,6 +47,32 @@ struct ListingDetailView: View {
             
             Divider()
             
+            //host info
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Entire villa hosted by John Smith")
+                        .font(.headline)
+                        .frame(width: 250, alignment: .leading)
+                    
+                    HStack(spacing: 2) {
+                        Text("4 guests -")
+                        Text("4 bedrooms -")
+                        Text("4 beds -")
+                        Text("3 baths")
+                    }
+                    .font(.caption)
+                }
+                .frame(width: 300, alignment: .leading)
+                
+                Spacer()
+                
+                Image("male-profile-photo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 64, height: 64)
+                    .clipShape(Circle())
+            }
+            .padding()
         }
     }
 }
