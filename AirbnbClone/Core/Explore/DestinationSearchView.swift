0192b1c8dbc5a34bb7623aa64243367220630036
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-11 02:31:13 -0400

    feat(search): implement expanded date selection view with DatePickers

--- AirbnbClone/Core/Explore/DestinationSearchView.swift
+++ AirbnbClone/Core/Explore/DestinationSearchView.swift
@@ -17,6 +17,9 @@ struct DestinationSearchView: View {
     @Binding var show: Bool
     @State private var destination = ""
     @State private var selectedOption: DestinationSearchOptions = .location
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    
     
     var body: some View {
         VStack {
@@ -72,19 +75,32 @@ struct DestinationSearchView: View {
             
             
             // date selection view
-            VStack {
+            VStack(alignment: .leading, spacing: 8) {
                 if selectedOption == .dates {
-                    HStack {
-                        Text("Show Expanded View")
+                    Text("When's your trip?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    VStack {
+                        DatePicker("From", selection: $startDate, displayedComponents: .date)
                         
-                        Spacer()
+                        Divider()
+                        
+                        DatePicker("To", selection: $endDate, displayedComponents: .date)
+
                     }
+                    .foregroundStyle(.secondary)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    
+                    
+                    
                 } else {
                     CollapsedPickerView(title: "When", description: "Add dates")
                 }
             }
             .padding()
-            .frame(height: selectedOption == .dates ? 120 : 64)
+            .frame(height: selectedOption == .dates ? 180 : 64)
             .background(.white)
             .clipShape(RoundedRectangle(cornerRadius: 12))
             .padding()
@@ -104,8 +120,8 @@ struct DestinationSearchView: View {
                         
                         Spacer()
                     }                } else {
-                    CollapsedPickerView(title: "Who", description: "Add guests")
-                }
+                        CollapsedPickerView(title: "Who", description: "Add guests")
+                    }
             }
             .padding()
             .frame(height: selectedOption == .guests ? 120 : 64)
