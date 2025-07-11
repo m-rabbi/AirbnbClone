e1025572cf053586d7810a1512cdd8596dca5468
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-11 06:11:41 -0400

    chore(explore): add ExploreViewModel skeleton with listings state and fetchListings placeholder

---
+++ AirbnbClone/Core/Explore/ViewModel/ExploreViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreViewModel.swift
+//  AirbnbClone
+//
+//  Created by Md Rabbi on 7/11/25.
+//
+
+import Foundation
+
+class ExploreViewModel: ObservableObject {
+    @Published var listings: [Listing] = []
+    
+    init() {
+        
+    }
+    
+    func fetchListings() async {
+        
+    }
+}
