813f388deba62f3265d9489be55699863f80f5e2
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-11 05:29:21 -0400

    feat(model): add Listing model with core listing properties

---
+++ AirbnbClone/Core/Listings/Model/Listing.swift
@@ -0,0 +1,29 @@
+//
+//  Listing.swift
+//  AirbnbClone
+//
+//  Created by Md Rabbi on 7/11/25.
+//
+
+import Foundation
+
+struct Listing: Identifiable, Codable {
+    let id: String
+    let ownerUid: String
+    let ownerName: String
+    let ownerImageUrl: String
+    let numberOfBedrooms: Int
+    let numberOfBathrooms: Int
+    let numberOfGuests: Int
+    let numberOfBeds: Int
+    var pricePerNight: Int
+    let latitude: Double
+    let longitude: Double
+    let address: String
+    let city: String
+    let state: String
+    let title: String
+    var rating: Double
+}
+
+
