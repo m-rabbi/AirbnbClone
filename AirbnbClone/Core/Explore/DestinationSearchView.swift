312486cc1e204c4281f597da3622b0b2ed2acd07
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-11 01:31:09 -0400

    feat(search): implement destination input field in DestinationSearchView

--- AirbnbClone/Core/Explore/DestinationSearchView.swift
+++ AirbnbClone/Core/Explore/DestinationSearchView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DestinationSearchView: View {
     @Binding var show: Bool
+    @State private var destination = ""
     
     var body: some View {
         VStack {
@@ -21,9 +22,43 @@ struct DestinationSearchView: View {
                     .imageScale(.large)
                     .foregroundStyle(.black)
             }
-
             
-            Text("Where to?")
+            // destination input
+            VStack(alignment: .leading) {
+                Text("Where to?")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .imageScale(.small)
+                    
+                    TextField("Search destinations", text: $destination)
+                        .font(.subheadline)
+                }
+                .frame(height: 44)
+                .padding(.horizontal)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 1)
+                        .foregroundStyle(Color(.systemGray4))
+                }
+            }
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            
+            // date selection view
+            VStack {
+                
+            }
+            
+            // num of guests
+            VStack {
+                
+            }
         }
     }
 }
