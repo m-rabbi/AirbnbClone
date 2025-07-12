3472f1f7c89a1b40dc8928fc19208f15760e3dbd
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-12 07:05:52 -0400

    feat(auth): create RegistrationView with styled input fields and primary button

--- AirbnbClone/Core/Authentication/Views/RegistrationView.swift
+++ AirbnbClone/Core/Authentication/Views/RegistrationView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var fullname = ""
+    
+    @Environment(\.dismiss) var dismiss
+     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            Image(.airbnbAppIcon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120, height: 120)
+                .padding()
+            
+            VStack {
+                TextField("Enter your email...", text: $email)
+                    .modifier(PrimaryTextFieldModifier())
+                
+                SecureField("Enter your password", text: $password)
+                    .modifier(PrimaryTextFieldModifier())
+                
+                TextField("Enter your full name...", text: $fullname)
+                    .modifier(PrimaryTextFieldModifier())
+            }
+                        
+            Button {
+                print("DEBUG: Create Account")
+            } label: {
+                Text("Create Account")
+                    .modifier(PrimaryButtonModifier())
+            }
+            .padding(.vertical, 8)
+            
+            Spacer()
+            
+            Divider()
+            
+            Button {
+                dismiss()
+            } label: {
+                HStack(spacing: 2) {
+                    Text("Already have an account?")
+                    
+                    Text("Sign in")
+                        .fontWeight(.semibold)
+                }
+                .font(.footnote)
+            }
+            .padding(.vertical)
+
+        }
     }
 }
 
